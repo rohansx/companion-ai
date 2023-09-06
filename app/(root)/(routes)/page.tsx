c4d61863f85edf098e75335981c967e69831b536
rohansx <sharmarohan615@gmail.com>
2023-09-07 02:49:02 +0530

    Add clerk authentication system

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+const RootPage = () => {
+    return ( 
+        <div>HEllo</div>
+        <UserButton />
+     );
+}
+ 
+export default RootPage;
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return ( 
-    <h1 className="text-green-500 text-3xl">Hello AI companion</h1>
-  )
-}
